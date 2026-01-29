8c2b44585aa7ce47929860acf091a71581b9c529
Ajay Sidal <ajay.sidal@opsvantagedigital.online>
2026-01-29 13:25:41 +1300

    Switch to @getsentry/nextjs for Next 16 compatibility

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/nextjs'
+import * as Sentry from '@getsentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/nextjs'
+import * as Sentry from '@getsentry/nextjs'
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || '',
